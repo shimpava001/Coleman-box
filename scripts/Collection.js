e4b0a7e0cb64ffa8c3c50a574c2254e78cbfe894
StellarCipher42 <SuvanS001@gmail.com>
2025-07-17 09:49:30 -0400

    almost finalized saving system

--- scripts/Collection.js
+++ scripts/Collection.js
@@ -15,7 +15,22 @@ for (let i = 0; i < blooks.length; i++) {
         let blookImg = document.querySelector(selector);
 
         if (blookImg) {
+            blookImg.classList.add('owned');
             blookImg.src = `Blooks/${Rarity}/${blookName}.png`;
+            blookImg.addEventListener("click", function () {
+                let SelectBlook = document.querySelector(".SelectBlook");
+                let rarity = document.querySelector(".rarity");
+                document.querySelector(".blookselect").textContent = blookName;
+                document.querySelector(".rarity").textContent = Rarity;
+                document.querySelector(".SelectBlook").src = `Blooks/${Rarity}/${blookName}.png`;
+                rarity.className = "rarity"
+                SelectBlook.classList.add('owned');
+                rarity.classList.add(Rarity.toLowerCase());
+                if (blookName === "Qintelligence Penguin") {
+                    let sound = new Audio('vids/noot noot.mp3');
+                    sound.play();
+                }
+            });
         }
     }
 }
